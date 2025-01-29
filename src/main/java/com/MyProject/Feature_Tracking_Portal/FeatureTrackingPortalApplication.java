b9db90563e2a771dcbb4be01ce865d5912990054
Ankii-sing <ankit9270791066@gmail.com>
2025-01-29 16:53:37 +0530

    initial commit

---
+++ src/main/java/com/MyProject/Feature_Tracking_Portal/FeatureTrackingPortalApplication.java
@@ -0,0 +1,13 @@
+package com.MyProject.Feature_Tracking_Portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureTrackingPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureTrackingPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MyProject/Feature_Tracking_Portal/FeatureTrackingPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyProject.Feature_Tracking_Portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureTrackingPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
